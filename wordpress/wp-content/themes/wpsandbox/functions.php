cdba427f9e4df6e34fa0eae1059b8f405c26e8c6
Jonathan Disla <jon@Mac-mini.local>
2022-07-26 23:47:37 -0400

    template files and readme

--- wordpress/wp-content/themes/wpsandbox/functions.php
+++ wordpress/wp-content/themes/wpsandbox/functions.php
@@ -1,3 +1,7 @@
 <?php
 
-add_action('wp_enqueue_scripts', '');
+// function wpsandbox_files() {
+//     wp_enqueue_style('style.css', get_template_directory_uri())
+// }
+
+// add_action('wp_enqueue_scripts', 'wpsandbox_files');
--- wordpress/wp-content/themes/wpsandbox/index.php
+++ wordpress/wp-content/themes/wpsandbox/index.php
@@ -1,13 +1,9 @@
 <?php
-get_header();
 
-while(have_posts()){
+while (have_posts()){
     the_post();?>
-    <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
-    <?php the_content(); ?>
-    <hr>
+    <h1><?php the_title(); ?></h1>
+    <p><?php the_excerpt(); ?></p>
 <?php }
-
-get_footer();
 
 ?>
